5abdd14b651be49045c099687735c213ac1daa83
Wahyu <wabredz1234@gmail.com>
2025-09-30 08:01:52 +0700

    update ulang

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import AppAdmin from "./pages/admin/AppAdmin.jsx";
-import Dashboard from "./pages/admin/Dashboard.jsx";
-import Pengguna from "./pages/admin/Pengguna.jsx";
-import Laporan from "./pages/admin/Laporan.jsx";
-import Profil from "./pages/Profil.jsx";
-import Matakuliah from "./pages/admin/MataKuliah.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 const router = createBrowserRouter([
@@ -25,36 +18,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Login />,
-      },
-      // {
-      //   path: "/register",
-      //   element: <Register />,
-      // },
-    ],
-  },
-  {
-    path: "/admin",
-    element: <AppAdmin />,
-    children: [
-      {
-        path: "/admin/dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "/admin/laporan",
-        element: <Laporan />,
-      },
-      {
-        path: "/admin/pengguna",
-        element: <Pengguna />,
-      },
-      {
-        path: "/admin/profil",
-        element: <Profil />,
-      },
-      {
-        path: "/admin/matakuliah",
-        element: <Matakuliah />,
       },
     ],
   },
