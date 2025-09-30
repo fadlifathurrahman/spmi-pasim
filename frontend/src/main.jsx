b74c92c3a7be06c1588dd4ab758108244bc4ccb9
Wahyu <wabredz1234@gmail.com>
2025-09-30 23:10:47 +0700

    tambah beberapa halaman

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,15 @@ import "./index.css";
 import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
 import NotFound from "./pages/NotFound.jsx";
+// import Dashboard from "./pages/Dashboard.jsx";
+// import StandarSubstandar from "./pages/StandarSubstandar.jsx";
+// import TargetIndikator from "./pages/TargetIndikator.jsx";
+// import TahunAkademikPeriode from "./pages/TahunAkademikPeriode.jsx";
+// import InputCapaian from "./pages/InputCapaian.jsx";
+// import ValidasiCapaian from "./pages/ValidasiCapaian.jsx";
+// import LaporanProdi from "./pages/LaporanProdi.jsx";
+// import LaporanAudit from "./pages/LaporanAudit.jsx";
+// import Evaluasi from "./pages/Evaluasi.jsx";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +28,42 @@ const router = createBrowserRouter([
         path: "/",
         element: <Login />,
       },
+      // {
+      //   path: "/dashboard",
+      //   element: <Dashboard />,
+      // },
+      // {
+      //   path: "/standar-substandar",
+      //   element: <StandarSubstandar />,
+      // },
+      // {
+      //   path: "/target-indikator",
+      //   element: <TargetIndikator />,
+      // },
+      // {
+      //   path: "/tahun-akademik-periode",
+      //   element: <TahunAkademikPeriode />,
+      // },
+      // {
+      //   path: "/evaluasi",
+      //   element: <Evaluasi />,
+      // },
+      // {
+      //   path: "/input-capaian",
+      //   element: <InputCapaian />,
+      // },
+      // {
+      //   path: "/validasi-capaian",
+      //   element: <ValidasiCapaian />,
+      // },
+      // {
+      //   path: "/laporan-prodi",
+      //   element: <LaporanProdi />,
+      // },
+      // {
+      //   path: "/laporan-audit",
+      //   element: <LaporanAudit />,
+      // },
     ],
   },
 ]);
