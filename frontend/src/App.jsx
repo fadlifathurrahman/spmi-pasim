7a485401c5adcd3830a2b2708554ceafe516efd5
Wahyu <wabredz1234@gmail.com>
2025-09-30 23:47:39 +0700

    update file App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import { Outlet } from "react-router-dom";
+import SideBar from "./components/Sidebar";
+import { useState } from "react";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
   return (
     <>
-      <Outlet />
+      <div className="flex h-screen overflow-hidden bg-gray-50 text-gray-900">
+        <SideBar isOpen={sidebarOpen} closeSidebar={closeSidebar} />
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <main className="flex-1 overflow-y-auto p-4">
+            <Outlet />
+          </main>
+        </div>
+      </div>
     </>
   );
 }
